8d5088a7300a24fe6ddd94383322ce21c17a5280
mojgang <mojgan.ghanavati@gmail.com>
2025-11-23 23:41:41 +1100

    Turn the site into a PWA

---
+++ static/sw.js
@@ -0,0 +1,12 @@
+// sw.js
+const CACHE = 'site-cache-v1';
+const FILES = [
+  '/', '/index.html', '/styles.css', '/main.js', '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
